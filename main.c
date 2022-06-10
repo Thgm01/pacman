2590fccd7289e6b635c09abd7e9cd813b79921c0
Thiago T. Moura <rpj134@gmail.com>
2022-06-10 11:43:24 -0300

    memory allocation for the map

--- main.c
+++ main.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+char** map;
+int lines, columns;
 
 void main()
 {
-    char map[5][11];
+
 
     FILE* fp = fopen("map.txt", "r");
     if(fp == NULL)
@@ -12,6 +14,16 @@ void main()
         printf("Error opening file\n");
         exit(1);
     }
+
+    fscanf(fp, "%d %d", &lines, &columns); //scan the number os lines and columns
+
+    //allocation suficient memory for the map 
+    map = malloc(sizeof(char*) * lines); //alloc the number of pointers (lines) 
+    for(int i = 0; i < lines; i++)
+    {
+        map[i] = malloc(sizeof(char) * columns+1); //alloc the number of chars in the pointer (columns)
+    }
+    
 
     for(int i = 0; i < 5; i++)
     {
@@ -23,5 +35,11 @@ void main()
         printf("%s\n", map[i]);
     }
     fclose(fp);
+
+    for(int i = 0; i < lines; i++)
+    {
+        free(map[i]);
+    }
+    free(map);
 
 }
